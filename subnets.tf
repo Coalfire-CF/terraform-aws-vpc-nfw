6523838f79efed3b54d8287ba4be295df375a8bc
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2024-01-08 15:49:51 -0500

    added new naming function to private

--- subnets.tf
+++ subnets.tf
@@ -46,7 +46,8 @@ resource "aws_subnet" "private" {
   availability_zone = element(var.azs, count.index)
 
   tags = merge(tomap({
-    "Name" = format("%s-${lower(element(values(var.private_subnet_name_tag), count.index))}-%s", var.name, element(var.azs, count.index))
+    #"Name" = format("%s-${lower(element(values(var.private_subnet_name_tag), count.index))}-%s", var.name, element(var.azs, count.index))
+    "Name" = format("%s-%s-%s", var.name, lookup(var.private_subnet_name_tag, element(split(".", split("/", cidr_block)[0]), 3)), element(var.azs, count.index))
   }), var.tags, var.private_subnet_tags)
 }
 
