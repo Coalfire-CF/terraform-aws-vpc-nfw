1377d9aa9319345d7b45fd9fd0d8c2fb069a09ec
kourosh-forti-hands <kourosh.mobl@coalfire.com>
2024-09-10 11:29:58 -0500

    testing fix for the public subnets

--- subnets.tf
+++ subnets.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = var.map_public_ip_on_launch
 
   tags = merge(tomap({
-    "Name" = format("%s-${lower(var.private_subnet_tags[count.index])}-%s", var.name, element(var.azs, count.index))
+    "Name" = format("%s-${lower(var.public_subnet_tags[count.index])}-%s", var.name, element(var.azs, count.index))
   }), var.tags)
 }
 
