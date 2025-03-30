eb35fb630736e8cabc74b78b65bd4dc9e667a8e0
kourosh-forti-hands <kourosh.mobl@coalfire.com>
2025-03-29 19:02:58 -0500

    working VPC endpoint code so far, needs a little testing - it does create them and adds them to the route tables. just need to validate connectivity mainly

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+locals {
+  max_subnet_length = max(length(var.private_subnets), length(var.elasticache_subnets), length(var.database_subnets), length(var.redshift_subnets), length(var.firewall_subnets), length(var.tgw_subnets))
+  nat_gateway_count = var.single_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)
+
+  nfw_subnets = [for s in aws_subnet.firewall : s.id]
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = element(
+  concat(aws_vpc_ipv4_cidr_block_association.this[*].vpc_id, aws_vpc.this[*].id, tolist([""])), 0)
+}
+
 ######
 # VPC
 ######
