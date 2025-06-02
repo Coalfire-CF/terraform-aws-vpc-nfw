b6be2e200613c95229bd41f0fc7d5d01358334d5
kourosh-forti-hands <kourosh.mobl@coalfire.com>
2025-06-02 18:48:42 -0500

    adding s3 flow logs because sometimes its needed

--- flowlog.tf
+++ flowlog.tf
@@ -82,3 +82,82 @@ resource "aws_iam_role_policy_attachment" "flowlogs_policy" {
   role       = aws_iam_role.flowlogs_role[0].name
   policy_arn = aws_iam_policy.flowlogs_policy[0].arn
 }
+
+resource "aws_s3_bucket" "flowlogs" {
+  count = var.flow_log_destination_type == "s3" ? 1 : 0
+  bucket = "${var.resource_prefix}-${var.aws_region}-flowlogs"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "flowlogs-encryption" {
+  count = var.flow_log_destination_type == "s3" ? 1 : 0
+  bucket = aws_s3_bucket.flowlogs[0].id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = var.s3_kms_key_arn
+    }
+  }
+}
+
+data "aws_iam_policy_document" "flowlogs_policy" {
+  statement {
+    actions = ["s3:GetBucketAcl"]
+    effect  = "Allow"
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    resources = [aws_s3_bucket.flowlogs[0].arn]
+  }
+
+  statement {
+    actions = ["s3:PutObject"]
+    effect  = "Allow"
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    resources = [
+    "${aws_s3_bucket.flowlogs[0].arn}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    actions = ["s3:GetObject", "s3:ListBucket"]
+    effect  = "Allow"
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+    resources = ["${aws_s3_bucket.flowlogs[0].arn}/*",
+    aws_s3_bucket.flowlogs[0].arn]
+  }
+}
+
+resource "aws_s3_bucket_policy" "flowlogs_bucket_policy" {
+  count = var.flow_log_destination_type == "s3" ? 1 : 0
+  bucket = aws_s3_bucket.flowlogs[0].bucket
+  policy = data.aws_iam_policy_document.flowlogs_policy.json
+}
+
+resource "aws_s3_bucket_public_access_block" "flowlogs" {
+  count = var.flow_log_destination_type == "s3" ? 1 : 0
+  bucket = aws_s3_bucket.flowlogs[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+resource "aws_s3_bucket_logging" "flowlogs" {
+  count = var.flow_log_destination_type == "s3" ? 1 : 0
+  bucket = aws_s3_bucket.flowlogs[0].id
+
+  target_bucket = var.s3_access_logs_bucket
+  target_prefix = "flowlogs/"
+}
