a82f12e47626a9fa79956d33ebcc8c4e76b3c33e
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-08-13 15:22:30 -0400

    fix: locals.tf subnet type label; subnet iteration method to properly handle empty sets

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
-  firewall_subnets              = tolist([for s in var.subnets : s if s.type == "firewall"])
-  public_subnets                = tolist([for s in var.subnets : s if s.type == "public"])
-  private_subnets               = tolist([for s in var.subnets : s if s.type == "private"])
-  tgw_subnets                   = tolist([for s in var.subnets : s if s.type == "tgw"])
-  database_subnets              = tolist([for s in var.subnets : s if s.type == "database"])
-  redshift_subnets              = tolist([for s in var.subnets : s if s.type == "redshift"])
-  elasticache_subnets           = tolist([for s in var.subnets : s if s.type == "elasticache"])
-  intra_subnets                 = tolist([for s in var.subnets : s if s.type == "firewall"])
+  firewall_subnets              = [for s in var.subnets : s if s.type == "firewall"]
+  public_subnets                = [for s in var.subnets : s if s.type == "public"]
+  private_subnets               = [for s in var.subnets : s if s.type == "private"]
+  tgw_subnets                   = [for s in var.subnets : s if s.type == "tgw"]
+  database_subnets              = [for s in var.subnets : s if s.type == "database"]
+  redshift_subnets              = [for s in var.subnets : s if s.type == "redshift"]
+  elasticache_subnets           = [for s in var.subnets : s if s.type == "elasticache"]
+  intra_subnets                 = [for s in var.subnets : s if s.type == "intra"]
   database_subnet_group_name    = "todo"
   redshift_subnet_group_name    = "todo"
   elasticache_subnet_group_name = "todo"
