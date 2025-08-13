30cd876416c69d4fcf562433454d9034b3c4a919
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-08-13 14:29:44 -0400

    explicitly type locals as maps

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
-  firewall_subnets              = [for s in var.subnets : s if s.type == "firewall"]
-  public_subnets                = [for s in var.subnets : s if s.type == "public"]
-  private_subnets               = [for s in var.subnets : s if s.type == "private"]
-  tgw_subnets                   = [for s in var.subnets : s if s.type == "tgw"]
-  database_subnets              = [for s in var.subnets : s if s.type == "database"]
-  redshift_subnets              = [for s in var.subnets : s if s.type == "redshift"]
-  elasticache_subnets           = [for s in var.subnets : s if s.type == "elasticache"]
-  intra_subnets                 = [for s in var.subnets : s if s.type == "firewall"]
+  firewall_subnets              = tomap([for s in var.subnets : s if s.type == "firewall"])
+  public_subnets                = tomap([for s in var.subnets : s if s.type == "public"])
+  private_subnets               = tomap([for s in var.subnets : s if s.type == "private"])
+  tgw_subnets                   = tomap([for s in var.subnets : s if s.type == "tgw"])
+  database_subnets              = tomap([for s in var.subnets : s if s.type == "database"])
+  redshift_subnets              = tomap([for s in var.subnets : s if s.type == "redshift"])
+  elasticache_subnets           = tomap([for s in var.subnets : s if s.type == "elasticache"])
+  intra_subnets                 = tomap([for s in var.subnets : s if s.type == "firewall"])
   database_subnet_group_name    = "todo"
   redshift_subnet_group_name    = "todo"
   elasticache_subnet_group_name = "todo"
