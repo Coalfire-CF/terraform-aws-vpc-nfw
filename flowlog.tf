bb52e33a326d0b830ccec31ab4e45ff4e1582e74
Brandon Donahoo <brandon.donahoo@coalfire.com>
2025-08-19 12:43:05 -0400

    feat: reduce prefix vars

--- flowlog.tf
+++ flowlog.tf
@@ -20,7 +20,7 @@ resource "aws_cloudwatch_log_group" "this" {
 
   name              = "/aws/vpcflow/${local.vpc_id}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
-  kms_key_id        = var.cloudwatch_log_group_kms_key_id
+  kms_key_id        = var.cloudwatch_log_group_kms_key_arn
   tags              = var.tags
 }
 
